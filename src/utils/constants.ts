3e5666597c8f237db1433611acc2c5d2c00d62d2
Balex <morevbalex777@gmail.com>
2025-03-19 15:44:32 +0300

    Initial commit for gh-pages branch

--- src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const API_URL = `https://larek-api.nomoreparties.co/api/weblarek`;
-export const CDN_URL = `https://larek-api.nomoreparties.co/content/weblarek`;
-
-export const settings = {};
